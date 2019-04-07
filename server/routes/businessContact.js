711c7915d637cbd4838b75a6060980b438a15724
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2019-04-06 22:44:29 -0400

    Business Contact field gets updated, login and logout at footer

--- server/routes/businessContact.js
+++ server/routes/businessContact.js
@@ -1,8 +1,8 @@
 /*
   File name: index.js
-  Author's name: Taera Kwon
-  StudentID: 300755802
-  Web App name: comp308_authentication
+  Author's name: Phuong Linh Pham
+  StudentID: 300923800
+  Web App name: comp308_asgn2
 */
 
 // Modules required for routing
